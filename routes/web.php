179130dd9f38aa82559e36781b8d7327aba03bc2
cintarmynti <ramayanticinta@gmail.com>
2025-08-29 14:21:49 +0700

    menyiapkan nama file

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\Frontend\ArtikelController;
+use App\Http\Controllers\Frontend\DokterController;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\KlinikController;
+use App\Http\Controllers\Frontend\LayananController;
+use App\Http\Controllers\Frontend\TestimonialController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Route untuk menu
+Route::get('/artikel', [ArtikelController::class, 'index'])->name('artikel.index');
+Route::get('/dokter', [DokterController::class, 'index'])->name('dokter.index');
+Route::get('/klinik', [KlinikController::class, 'index'])->name('klinik.index');
+Route::get('/layanan', [LayananController::class, 'index'])->name('layanan.index');
+Route::get('/testimonial', [TestimonialController::class, 'index'])->name('testimonial.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
